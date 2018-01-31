150bb8586b85b2fb64bb2438018eed43e9032461
Charles Chen <284636917@qq.com>
2018-01-31 15:29:41 +0800

    Simple Spring Boot Framework Establishment

---
+++ src/main/java/com/aliclound/Application.java
@@ -0,0 +1,19 @@
+package com.aliclound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the entry point of this demo app
+ *
+ * @author hao.e.chen
+ * @date 31/01/2018
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/aliclound/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.aliclound.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is the home controller for accessing the home page e.g.
+ * @author hao.e.chen
+ * @date 31/01/2018
+ */
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public ResponseEntity home(){
+        return ResponseEntity.ok("Welcome to Charles Ali Cloud home page.");
+    }
+}
