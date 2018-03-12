794a1c1dd80c1a95ae2a1743c41374d31adf049b
Charles Chen <284636917@qq.com>
2018-03-12 10:03:40 +0800

    CI/CD integration - test the web hook 2

--- src/main/java/com/alicloud/controller/HomeController.java
+++ src/main/java/com/alicloud/controller/HomeController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public ResponseEntity home(){
-        return ResponseEntity.ok("Welcome to SmartZone.");
+        return ResponseEntity.ok("Welcome to SmartZone of IT Expert.");
     }
 }
